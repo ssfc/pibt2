37d87470a3668a183a5e892a9efe5e0522aa296f
SSFC <sortsimilar@gmail.com>
2025-06-30 21:18:50 +0800

    int small_region_column = 12; // (errand点)(小)每个region所占的列数

--- pibt2/include/pibtm.hpp
+++ pibt2/include/pibtm.hpp
@@ -18,6 +18,14 @@ private:
     int x, y;
   };
 
+  int small_region_column = 12; // (errand点)(小)每个region所占的列数
+  int small_region_row = 12; // (errand点)(小)每个region所占的行数
+  // (历史统计)地图有几列small region
+  int num_small_region_column;
+  // (历史统计)地图有几行small region
+  int num_small_region_row;
+
+
   // PIBT agent
   struct Agent {
     int id;
