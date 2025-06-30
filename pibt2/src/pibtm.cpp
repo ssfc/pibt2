e8e7d32e4eefefc31c9309163ddae2b6ba64f501
SSFC <sortsimilar@gmail.com>
2025-06-30 21:09:59 +0800

    if(timestep % 20 == 0)

--- pibt2/src/pibtm.cpp
+++ pibt2/src/pibtm.cpp
@@ -178,7 +178,7 @@ void PIBTM::run()
   while (true)
   {
 
-    if(timestep % 50 == 0)
+    if(timestep % 20 == 0)
     {
       info(" ", "elapsed:", getSolverElapsedTime(), ", timestep:", timestep);
 
