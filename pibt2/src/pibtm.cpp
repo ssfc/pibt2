ed444368e697706ff16b50965fa894a62c71f159
SSFC <sortsimilar@gmail.com>
2025-06-30 18:22:38 +0800

    // std::sort(A.begin(), A.end(), compare); // 按优先级排序

--- pibt2/src/pibtm.cpp
+++ pibt2/src/pibtm.cpp
@@ -166,6 +166,7 @@ void PIBTM::run()
     }
 
     // planning
+    // std::sort(A.begin(), A.end(), compare); // 按优先级排序
     std::sort(A.begin(), A.end(), compare_addition); // 按优先级排序
     for (auto a : A)
     {
