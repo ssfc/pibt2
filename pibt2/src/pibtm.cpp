7ec5208c5b9bd17d0b89f393e65f450eb5dd21bd
SSFC <sortsimilar@gmail.com>
2025-06-30 09:16:29 +0800

    // 优先比较: 已耗时越多，优先级越高（先被决策）。

--- pibt2/src/pibtm.cpp
+++ pibt2/src/pibtm.cpp
@@ -24,11 +24,20 @@ void PIBTM::run()
 {
   // compare priority of agents
   auto compare = [](Agent* a, const Agent* b) {
-    // 已耗时越多，优先级越高（先被决策）。
-    if (a->elapsed != b->elapsed) return a->elapsed > b->elapsed;
-    // 初始距离越远，优先级越高。
+    // 优先比较: 已耗时越多，优先级越高（先被决策）。
+    if (a->elapsed != b->elapsed)
+    {
+      return a->elapsed > b->elapsed;
+    }
+
+    // 次级比较: 初始距离越远，优先级越高。
     // use initial distance
-    if (a->init_d != b->init_d) return a->init_d > b->init_d;
+    if (a->init_d != b->init_d)
+    {
+      return a->init_d > b->init_d;
+    }
+
+    // 最后比较: 0-1随机数
     return a->tie_breaker > b->tie_breaker; // 随机值作为打破平手的最后手段。
   };
 
