99326ca89fbfbbbbd00c169f61b77aaad5346f7e
SSFC <sortsimilar@gmail.com>
2025-07-01 15:42:00 +0800

    if (a->region_mainstream_inner_product != b->region_mainstream_inner_product)

--- pibt2/src/pibtm.cpp
+++ pibt2/src/pibtm.cpp
@@ -232,9 +232,9 @@ void PIBTM::run()
   {
     // 次级比较: 初始距离越远，优先级越高。
     // use initial distance
-    if (a->mainstream_inner_product != b->mainstream_inner_product)
+    if (a->region_mainstream_inner_product != b->region_mainstream_inner_product)
     {
-      return a->mainstream_inner_product > b->mainstream_inner_product;
+      return a->region_mainstream_inner_product > b->region_mainstream_inner_product;
     }
 
     // 优先比较: 已耗时越多，优先级越高（先被决策）。
