66986f2cfb9faddd66be74979762687b365a327e
SSFC <sortsimilar@gmail.com>
2025-07-02 15:12:30 +0800

    static bool compare(const Agent* a, const Agent* b)

--- pibt2/include/pibtm.hpp
+++ pibt2/include/pibtm.hpp
@@ -63,6 +63,33 @@ private:
   // main
   void run();
 
+  static bool compare(const Agent* a, const Agent* b)
+  {
+    // 次级比较: 初始距离越远，优先级越高。80%概率给优先级, 不能完全不给随机
+    // 内积的方法能否完全刻画agent和主流的距离
+    // use initial distance
+    if (a->mainstream_inner_product != b->mainstream_inner_product)
+    {
+      return a->mainstream_inner_product > b->mainstream_inner_product;
+    }
+
+    // 优先比较: 已耗时越多，优先级越高（先被决策）。80%概率给优先级, 不能完全不给随机; 如果完全找不到路再降一些
+    if (a->elapsed != b->elapsed)
+    {
+      return a->elapsed > b->elapsed;
+    }
+
+    // 次级比较: 初始距离越远，优先级越高。
+    // use initial distance
+    if (a->init_d != b->init_d)
+    {
+      return a->init_d > b->init_d;
+    }
+
+    // 最后比较: 0-1随机数
+    return a->tie_breaker > b->tie_breaker; // 随机值作为打破平手的最后手段。
+  };
+
 public:
   PIBTM(MAPF_Instance* _P);
   ~PIBTM() {}
