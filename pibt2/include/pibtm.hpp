4d69a0da4d2ca4410e517f65fd59441933cbcfc1
SSFC <sortsimilar@gmail.com>
2025-07-02 15:21:50 +0800

    261	Move compare_cos. (2025年7月2日)

--- pibt2/include/pibtm.hpp
+++ pibt2/include/pibtm.hpp
@@ -90,6 +90,33 @@ private:
     return a->tie_breaker > b->tie_breaker; // 随机值作为打破平手的最后手段。
   };
 
+  // compare priority of agents
+  static bool compare_cos(const Agent* a, const Agent* b)
+  {
+    // 次级比较: 初始距离越远，优先级越高。
+    // use initial distance
+    if (a->mainstream_cos != b->mainstream_cos)
+    {
+      return a->mainstream_cos > b->mainstream_cos;
+    }
+
+    // 优先比较: 已耗时越多，优先级越高（先被决策）。
+    if (a->elapsed != b->elapsed)
+    {
+      return a->elapsed > b->elapsed;
+    }
+
+    // 次级比较: 初始距离越远，优先级越高。
+    // use initial distance
+    if (a->init_d != b->init_d)
+    {
+      return a->init_d > b->init_d;
+    }
+
+    // 最后比较: 0-1随机数
+    return a->tie_breaker > b->tie_breaker; // 随机值作为打破平手的最后手段。
+  };
+
 public:
   PIBTM(MAPF_Instance* _P);
   ~PIBTM() {}
