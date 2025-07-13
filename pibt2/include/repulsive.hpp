24257efd433d10a604f72ed57906a09140823acc
SSFC <sortsimilar@gmail.com>
2025-07-13 20:15:34 +0800

    #include "solver.hpp"

--- pibt2/include/repulsive.hpp
+++ pibt2/include/repulsive.hpp
@@ -5,4 +5,45 @@
 #ifndef REPULSIVE_HPP
 #define REPULSIVE_HPP
 
+#include "solver.hpp"
+
+class PIBTR : public MAPF_Solver
+{
+public:
+  static const std::string SOLVER_NAME;
+
+private:
+  // PIBT agent
+  struct Agent {
+    int id;
+    Node* v_now;        // current location
+    Node* v_next;       // next location
+    Node* g;            // goal
+    int elapsed;        // eta
+    int init_d;         // initial distance
+    float tie_breaker;  // epsilon, tie-breaker
+  };
+  using Agents = std::vector<Agent*>;
+
+  // <node-id, agent>, whether the node is occupied or not
+  // work as reservation table
+  Agents occupied_now;
+  Agents occupied_next;
+
+  // option
+  bool disable_dist_init = false;
+
+  // result of priority inheritance: true -> valid, false -> invalid
+  bool funcPIBT(Agent* ai, Agent* aj = nullptr);
+
+  // main
+  void run();
+
+public:
+  PIBTR(MAPF_Instance* _P);
+  ~PIBTR() {}
+
+  void setParams(int argc, char* argv[]);
+  static void printHelp();
+};
 #endif  // REPULSIVE_HPP
