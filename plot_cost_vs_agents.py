e896af25819e53549794bd52f87afd2d346aa223
SSFC <sortsimilar@gmail.com>
2025-09-30 10:15:53 +0800

    ("PIBT-allTimeFlow-reverse", {"high level planner": "no", "low level planner": "PIBT-allTimeFlow-reverse"}, {"marker": "^"}),

--- plot_cost_vs_agents.py
+++ plot_cost_vs_agents.py
@@ -74,8 +74,9 @@ def main():
 
     # 不同方法的附加条件与样式
     methods: Tuple[Tuple[str, Dict[str, object], Dict[str, object]], ...] = (
-        ("PIBT", {"high level planner": "no", "low level planner": "PIBT"}, {"marker": "^"}),
+        #("PIBT", {"high level planner": "no", "low level planner": "PIBT"}, {"marker": "^"}),
         ("PIBT-allTimeFlow", {"high level planner": "no", "low level planner": "PIBT-allTimeFlow"}, {"marker": "v"}),
+        ("PIBT-allTimeFlow-reverse", {"high level planner": "no", "low level planner": "PIBT-allTimeFlow-reverse"}, {"marker": "^"}),
         #("CBS", {"high level planner": "CBS-whoenig"}, {"marker": "^"}),
         #("CBSDepth", {"high level planner": "CBSDepth-whoenig"}, {"marker": "v"}),
         #("CBSDepthKNN", {"high level planner": "CBSDepthKNN"}, {"marker": "^"}),
